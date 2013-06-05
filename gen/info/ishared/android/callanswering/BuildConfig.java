d133d54e3c1dbfd8d8c67fe56b9ba6932e5e034e
Seven <lixfeng.3@gmail.com>
2013-06-05 15:38:11 +0800

    First commit

---
+++ gen/info/ishared/android/callanswering/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package info.ishared.android.callanswering;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
